6a0e982b284bfc4ea6ea6179ab3b5ae44a4712e4
USER <USER@DESKTOP-FUGEFD6>
2023-02-11 12:09:01 +0530

    commit

---
+++ Automation1/src/qsp/Demo.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Demo {
+static {
+System.setProperty("webdriver.chrome.driver", "C:\\Users\\USER\\eclipse-workspace\\Automation1\\driver\\chromedriver.exe");
+}
+	public static void main(String[] args) {
+	WebDriver driver=new ChromeDriver();
+
+	}
+
+}
